a47e16babe73f3ce96570e5fb3eb433d36122c93
dhonstudio <doon13@gmail.com>
2021-10-26 15:03:07 +0700

    Add Class Layout ListView Card with Tap

--- lib/layoutlistview.dart
+++ lib/layoutlistview.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
 
 import 'package:flutter/material.dart';
 
@@ -9,7 +9,7 @@ class LayoutListView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Layout ListView'),
       ),
-      body: MyListView(),
+      body: MyListViewCard(),
     );
   }
 }
@@ -21,7 +21,7 @@ class MyListView extends StatelessWidget {
       padding: EdgeInsets.all(12.0),
       children: <Widget>[
         ListTile(
-          title: Text('List 1'),
+          title: Text('Home'),
           subtitle: Text('This is Homepage'),
           leading: Icon(Icons.home),
           trailing: Icon(Icons.bookmark),
@@ -52,5 +52,41 @@ class MyListView extends StatelessWidget {
         ),
       ],
     ));
+  }
+}
+
+class MyListViewCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(8.0),
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            onTap: () {
+              print("Klik Github");
+            },
+            title: Text('My Github'),
+            subtitle: Text('Github Homepage'),
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/46585919?v=4'),
+            ),
+            trailing: Icon(Icons.bookmark_outline),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('Alex Suprun'),
+            subtitle: Text('Photo of Alex'),
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=580&q=80'),
+            ),
+            trailing: Icon(Icons.bookmark_outline),
+          ),
+        )
+      ],
+    );
   }
 }
